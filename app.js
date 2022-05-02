dca72715f3daa2da552ef361a6481a729294e5fb
vijay Kumar <vijay.kumar@absolutefoods.in>
2022-05-02 21:42:56 +0530

    changed var to const

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var apiRoute = require('./routes/apiRoute');
+const apiRoute = require('./routes/apiRoute');
 
-var app = express();
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 const v1 = require('./v1/v1.router')
 const v2 = require('./v2/v2.router')
+
+const router = express.Router();
+
+
 /* GET home page. */
 router.use('/api', v1)
 router.use('/api', v2)
